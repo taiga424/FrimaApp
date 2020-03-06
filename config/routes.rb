81cd108e0ff083272a049b1de12496491408dc47
Tomoya-Sonok <wlcmty08kh@gmail.com>
2020-03-06 17:00:28 +0900

    [create]purchase front-page

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,9 @@ Rails.application.routes.draw do
   root 'items#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :items, only:[:show, :new, :create]
+  resources :items, only:[:show, :new, :create] do
+    member do
+      get :confirm
+    end
+  end
 end
