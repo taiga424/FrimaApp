405e5c246be9e746bbd4a1162ebf0e3d0bbe6dba
SomaSekimoto <0421.soma@gmail.com>
2020-03-06 16:45:48 +0900

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_05_095150) do
+ActiveRecord::Schema.define(version: 2020_03_06_064210) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "prefecture", null: false
@@ -64,4 +64,5 @@ ActiveRecord::Schema.define(version: 2020_03_05_095150) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "images", "items"
 end
