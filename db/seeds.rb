53e1472f1e7e68d7fbe1e1869c1ffa330108e4e9
taiga424 <taiga-hand4@ezweb.ne.jp>
2020-03-09 13:43:29 +0900

    seed.rb create

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,100 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Item.Create!(
+  [
+    {
+      name: "パーカー"
+      description: "パーカーです"
+      price: 1000
+      brand:
+      condition:
+      image: File.open('.app/assets/images/clothes1')
+      fee: 100
+      area: 3
+      shipping_days: 1
+      buyer:
+    },
+    {
+      name: "スウェット"
+      description: "スウェットです"
+      price: 2000
+      brand:
+      condition:
+      image: File.open('.app/assets/images/clothes2')
+      fee: 200
+      area: 2
+      shipping_days: 2
+      buyer: 1
+    },
+    {
+      name: "ワンピース"
+      description: "ワンピースです"
+      price: 3000
+      brand:
+      condition:
+      image: File.open('.app/assets/images/clothes3')
+      fee: 300
+      area: 3
+      shipping_days: 3
+      buyer:
+    },
+    {
+      name: "靴"
+      description: "靴です"
+      price: 4000
+      brand:
+      condition:
+      image: File.open('.app/assets/images/clothes4')
+      fee: 200
+      area: 3
+      shipping_days: 2
+      buyer: 2
+    },
+    {
+      name: "子供服"
+      description: "子供服です"
+      price: 5000
+      brand:
+      condition:
+      image: File.open('.app/assets/images/clothes5')
+      fee: 100
+      area: 2
+      shipping_days: 1
+      buyer:
+    },
+    {
+      name: "スカート"
+      description: "スカートです"
+      price: 6000
+      brand:
+      condition:
+      image: File.open('.app/assets/images/clothes6')
+      fee: 500
+      area: 1
+      shipping_days: 3
+      buyer: 1
+    },
+    {
+      name: "バッグ"
+      description: "バッグです"
+      price: 2000
+      brand:
+      condition:
+      image: File.open('.app/assets/images/clothes7')
+      fee: 200
+      area: 1
+      shipping_days: 4
+      buyer:
+    },
+    {
+      name: "パンツ"
+      description: "パンツです"
+      price: 6000
+      brand:
+      condition:
+      image: File.open('.app/assets/images/clothes8')
+      fee: 400
+      area: 2
+      shipping_days: 2
+      buyer: 2
+    },
+  ]
+)
