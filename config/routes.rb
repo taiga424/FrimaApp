4f5309bd81b915dfd3377f121788038471fe1e5f
kaitonishimura624 <kaito.uw@gmail.com>
2020-03-09 13:03:34 +0900

    mypage-show-done

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,14 @@ Rails.application.routes.draw do
   root 'items#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+
   resources :items, only:[:show, :new, :create] do
     member do
       get :confirm
     end
   end
+
+  resources :users, only: [:index, :show]
+  resources :items, only:[:show, :new, :create]
+
 end
