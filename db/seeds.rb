9f01d2959c261ad065f12d61ebb4d248d90acbcf
Tomoya-Sonok <wlcmty08kh@gmail.com>
2020-03-10 20:05:46 +0900

    updated seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,112 +1,4 @@
 # coding: utf-8
-Item.create!(
-  [
-    {
-      name: 'パーカー',
-      description: 'パーカーです',
-      price: 1000,
-      condition: '',
-      fee: 1,
-      area: 3,
-      shipping_days: 1,
-      buyer: 2,
-    },
-    # {
-    #   name: 'スウェット',
-    #   description: 'スウェットです',
-    #   price: 2000,
-    #   condition: '',
-    #   image: 'clothes2.jpeg',
-    #   fee: 200 ,
-    #   area: 2,
-    #   shipping_days: 2 ,
-    #   buyer: 1 ,
-    # },
-    {
-      name: 'ワンピース',
-      description: 'ワンピースです',
-      price: 3000,
-      condition: '',
-      fee: 2,
-      area: 3,
-      shipping_days: 3,
-      buyer: '',
-    },
-    {
-      name: '靴',
-      description: '靴です',
-      price: 4000,
-      condition: '',
-      fee: 2,
-      area: 3,
-      shipping_days: 2,
-      buyer: 2,
-    },
-    {
-      name: '子供服',
-      description: '子供服です',
-      price: 5000,
-      condition: '',
-      fee: 1,
-      area: 2,
-      shipping_days: 1,
-      buyer: '',
-    },
-    {
-      name: 'スカート',
-      description: 'スカートです',
-      price: 6000,
-      condition: '',
-      fee: 1,
-      area: 1,
-      shipping_days: 3,
-      buyer: 1,
-    },
-    {
-      name: 'バッグ',
-      description: 'バッグです',
-      price: 2000,
-      condition: '',
-      fee: 2,
-      area: 1,
-      shipping_days: 4,
-      buyer: '',
-    },
-    # {
-    #   name: 'パンツ',
-    #   description: 'パンツです',
-    #   price: 6000,
-    #   condition: '',
-    #   fee: 1,
-    #   area: 2,
-    #   shipping_days: 2,
-    #   buyer: 2,
-    # },
-  ]
-)
-
-Image.create!(
-  [
-    {
-      content: 'clothes1.jpeg'
-    },
-    {
-      content: 'clothes3.jpeg'
-    },
-    {
-      content: 'clothes4.jpeg'
-    },
-    {
-      content: 'clothes5.jpeg'
-    },
-    {
-      content: 'clothes6.jpeg'
-    },
-    {
-      content: 'clothes7.jpeg'
-    },
-  ]
-)
 User.create!(
   [
     {
@@ -174,6 +66,132 @@ Brand.create!(
     },
     {
       name: 'CONVERSE'
+    },
+  ]
+)
+Item.create!(
+  [
+    {
+      name: 'パーカー',
+      description: 'パーカーです',
+      price: 1000,
+      condition: '',
+      fee: 1,
+      area: 3,
+      shipping_days: 1,
+      buyer: 2,
+      brand_id: 1,
+      user_id: 1,
+    },
+    # {
+    #   name: 'スウェット',
+    #   description: 'スウェットです',
+    #   price: 2000,
+    #   condition: '',
+    #   image: 'clothes2.jpeg',
+    #   fee: 200 ,
+    #   area: 2,
+    #   shipping_days: 2 ,
+    #   buyer: 1 ,
+    # },
+    {
+      name: 'ワンピース',
+      description: 'ワンピースです',
+      price: 3000,
+      condition: '',
+      fee: 2,
+      area: 3,
+      shipping_days: 3,
+      buyer: '',
+      brand_id: 2,
+      user_id: 1,
+    },
+    {
+      name: '靴',
+      description: '靴です',
+      price: 4000,
+      condition: '',
+      fee: 2,
+      area: 3,
+      shipping_days: 2,
+      buyer: 2,
+      brand_id: 3,
+      user_id: 1,
+    },
+    {
+      name: '子供服',
+      description: '子供服です',
+      price: 5000,
+      condition: '',
+      fee: 1,
+      area: 2,
+      shipping_days: 1,
+      buyer: '',
+      brand_id: 4,
+      user_id: 2,
+    },
+    {
+      name: 'スカート',
+      description: 'スカートです',
+      price: 6000,
+      condition: '',
+      fee: 1,
+      area: 1,
+      shipping_days: 3,
+      buyer: 1,
+      brand_id: 5,
+      user_id: 3,
+    },
+    {
+      name: 'バッグ',
+      description: 'バッグです',
+      price: 2000,
+      condition: '',
+      fee: 2,
+      area: 1,
+      shipping_days: 4,
+      buyer: '',
+      brand_id: 6,
+      user_id: 4,
+    },
+    # {
+    #   name: 'パンツ',
+    #   description: 'パンツです',
+    #   price: 6000,
+    #   condition: '',
+    #   fee: 1,
+    #   area: 2,
+    #   shipping_days: 2,
+    #   buyer: 2,
+    # },
+  ]
+)
+
+Image.create!(
+  [
+    {
+      content: 'clothes1.jpeg',
+      item_id: 1,
+    },
+    {
+      content: 'clothes3.jpeg',
+      item_id: 2,
+    },
+    {
+      content: 'clothes4.jpeg',
+      item_id: 3,
+    },
+    {
+      content: 'clothes5.jpeg',
+      item_id: 4,
+    },
+    {
+      content: 'clothes6.jpeg',
+      item_id: 5,
+    },
+    {
+      content: 'clothes7.jpeg',
+      item_id: 6,
     },
   ]
 )
