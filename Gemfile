8ff63d0341ae4c56a82f97abe1c0d790f765f76f
kaitonishimura624 <kaito.uw@gmail.com>
2020-03-11 14:12:57 +0900

    setting-payment

--- Gemfile
+++ Gemfile
@@ -78,4 +78,6 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem "haml-rails", ">= 1.0", '<= 2.0.1'
 gem "font-awesome-sass"
 gem 'devise'
-
+gem 'payjp'
+gem "jquery-rails"
+gem 'dotenv-rails'
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,16 @@
 Rails.application.routes.draw do
+
   devise_for :users
   root 'items#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  resources :card, only: [:new, :show] do
+    collection do
+      post 'show', to: 'card#show'
+      post 'pay', to: 'card#pay'
+      post 'delete', to: 'card#delete'
+    end
+  end
 
   resources :items, only:[:show, :new, :create, :edit, :destroy] do
     member do
@@ -11,5 +19,13 @@ Rails.application.routes.draw do
   end
 
   resources :users, only: [:index, :show]
+
+  resources :purchase, only: [:index] do
+    collection do
+      get 'index', to: 'purchase#index'
+      post 'pay', to: 'purchase#pay'
+      get 'done', to: 'purchase#done'
+    end
+  end
 
 end
