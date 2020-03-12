9c7d6dc168f51398e5c2865fae36ab9c6117be10
kaitonishimura624 <kaito.uw@gmail.com>
2020-03-12 21:43:33 +0900

    testforuser

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  
-  has_many :items
+         has_many :items
+  validates :nickname, presence: true
+  validates :encrypted_password, presence: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :furigana_first, presence: true
+  validates :furigana_last, presence: true
+  validates :birthdate, presence: true
+  validates :encrypted_password, presence: true, length: { maximum: 7 }
+
 end
+
