1e13e0bed94b35413de9a29c818d8110184064f6
Tomoya-Sonok <wlcmty08kh@gmail.com>
2020-03-12 11:59:23 +0900

    a few modifications for payjp & category-function

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -44,6 +44,7 @@ class ItemsController < ApplicationController
     :customer => card.customer_id, #顧客ID
     :currency => 'jpy', #日本円
       )
+    @item.update( customer_id: current_user.id)
     redirect_to action: 'done' #完了画面に移動
   end
 
