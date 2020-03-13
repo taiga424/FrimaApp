b88d8e1f4da85e10c44d6a2ada48a6c499469077
Tomoya-Sonok <wlcmty08kh@gmail.com>
2020-03-13 16:57:43 +0900

    fix for deployment

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -44,7 +44,7 @@ class ItemsController < ApplicationController
     :customer => card.customer_id, #顧客ID
     :currency => 'jpy', #日本円
       )
-    @item.update( customer_id: current_user.id)
+    @item.update( purchase_id: current_user.id)
     redirect_to action: 'done' #完了画面に移動
   end
 
