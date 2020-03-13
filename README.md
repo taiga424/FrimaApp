2942adba739644743e696a826c9fb482915a2d51
ryogosato <60603711+ryogosato@users.noreply.github.com>
2020-03-13 15:46:14 +0900

    rename, change_not_null

--- README.md
+++ README.md
@@ -67,7 +67,7 @@ Things you may want to cover:
 |fee|integer|null: false|
 |area|string|null: false|
 |shipping_days|integer|null: false|
-|customer_id|integer|foreign_key: true|
+|purchase_id|integer|foreign_key: true|
 |brand_id|bigint|foreign_key: true|
 |user_id|bigint|foreign_key: true|
 |category_id|bigint|foreign_key: true|
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_11_050937) do
+ActiveRecord::Schema.define(version: 2020_03_13_064138) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "prefecture", null: false
@@ -49,11 +49,11 @@ ActiveRecord::Schema.define(version: 2020_03_11_050937) do
     t.string "name", null: false
     t.text "description", null: false
     t.integer "price", null: false
-    t.integer "condition"
+    t.integer "condition", null: false
     t.integer "fee", null: false
     t.integer "area", null: false
     t.integer "shipping_days", null: false
-    t.string "customer_id"
+    t.string "purchase_id"
     t.bigint "brand_id"
     t.bigint "user_id"
     t.bigint "category_id", null: false
