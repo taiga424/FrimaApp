964e496429255f843c8baf944efa48e6f9496e0d
Tomoya-Sonok <wlcmty08kh@gmail.com>
2020-03-16 21:20:00 +0900

    slightly modified migration-file

--- db/migrate/20200316073842_add_postalcode_to_addresses.rb
+++ db/migrate/20200316073842_add_postalcode_to_addresses.rb
@@ -1,6 +1,6 @@
 class AddPostalcodeToAddresses < ActiveRecord::Migration[5.2]
   def change
-    add_ :addresses, :postal_code, :integer, null: false
+    add_column :addresses, :postal_code, :integer, null: false
   end
 end
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2020_03_16_092642) do
-
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "prefecture", null: false
@@ -95,7 +93,6 @@ ActiveRecord::Schema.define(version: 2020_03_16_092642) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
-
 
   add_foreign_key "addresses", "users"
   add_foreign_key "comments", "items"
