8228933f5d8907be76877415940168a6f2d1995b
Tomoya-Sonok <wlcmty08kh@gmail.com>
2020-03-17 13:59:44 +0900

    Update items_controller.rb

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -10,6 +10,8 @@ class ItemsController < ApplicationController
   end
 
   def show
+    @comment = Comment.new
+    @comments = @item.comments.includes(:user)
     @user = User.find_by(id: @item.user_id)
     @images = Image.where(item_id: @item.id)
     @shipping_day = @item.shipping_days
