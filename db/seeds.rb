ad2ecbcdbd6991ebb55fa5c24bfc7f8ffcf16e76
Tomoya-Sonok <wlcmty08kh@gmail.com>
2020-03-17 13:26:44 +0900

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -156,27 +156,27 @@ Item.create!(
 Image.create!(
   [
     {
-      content: File.open('clothes1.jpeg'),
+      content: File.open('app/assets/images/clothes1.jpeg'),
       item_id: 1,
     },
     {
-      content: File.open('clothes3.jpeg'),
+      content: File.open('app/assets/images/clothes3.jpeg'),
       item_id: 2,
     },
     {
-      content: File.open('clothes4.jpeg'),
+      content: File.open('app/assets/images/clothes4.jpeg'),
       item_id: 3,
     },
     {
-      content: File.open('clothes5.jpeg'),
+      content: File.open('app/assets/images/clothes5.jpeg'),
       item_id: 4,
     },
     {
-      content: File.open('clothes6.jpeg'),
+      content: File.open('app/assets/images/clothes6.jpeg'),
       item_id: 5,
     },
     {
-      content: File.open('clothes7.jpeg'),
+      content: File.open('app/assets/images/clothes7.jpeg'),
       item_id: 6,
     },
   ]
