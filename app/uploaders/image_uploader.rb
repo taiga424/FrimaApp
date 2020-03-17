d4810c502f8e81e5182b1a0c36ecc23dda291925
Tomoya-Sonok <wlcmty08kh@gmail.com>
2020-03-17 19:32:59 +0900

    configured S3

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -4,8 +4,18 @@ class ImageUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
-  # storage :fog
+  # storage :file
+  storage :fog
+  # if Rails.env.development?
+  #   # ローカルに保存する
+  #   storage :file
+  # elsif Rails.env.test?
+  #   # ローカルに保存する
+  #   storage :file
+  # else
+  #   # S3などの外部に保存する
+  #   storage :fog
+  # end
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
