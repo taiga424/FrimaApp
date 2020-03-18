cd7e823e77cb37146b86a24eddf505bca9dc3d0e
taiga424 <taiga-hand4@ezweb.ne.jp>
2020-03-18 14:12:59 +0900

    add address.rb

---
+++ db/seeds/image.rb
@@ -0,0 +1,36 @@
+Image.create!(
+  [
+    {
+      content: File.open('app/assets/images/clothes1.jpeg'),
+      item_id: 1,
+    },
+    {
+      content: File.open('app/assets/images/clothes3.jpeg'),
+      item_id: 1,
+    },
+    {
+      content: File.open('app/assets/images/clothes4.jpeg'),
+      item_id: 1,
+    },
+    {
+      content: File.open('app/assets/images/clothes3.jpeg'),
+      item_id: 2,
+    },
+    {
+      content: File.open('app/assets/images/clothes4.jpeg'),
+      item_id: 3,
+    },
+    {
+      content: File.open('app/assets/images/clothes5.jpeg'),
+      item_id: 4,
+    },
+    {
+      content: File.open('app/assets/images/clothes6.jpeg'),
+      item_id: 5,
+    },
+    {
+      content: File.open('app/assets/images/clothes7.jpeg'),
+      item_id: 6,
+    },
+  ]
+)
---
+++ db/seeds/user.rb
@@ -0,0 +1,48 @@
+User.create!(
+  [
+    {
+      nickname: 'taro',
+      email: 't1@taro.com',
+      first_name: '太郎',
+      last_name: '山田',
+      furigana_first: 'タロウ',
+      furigana_last: 'ヤマダ',
+      birthdate: '2010-07-11',
+      tell: '00000000000',
+      password: "taro123"
+    },
+    {
+      nickname: 'jiro',
+      email: 'te2@jiro.com',
+      first_name: '二郎',
+      last_name: '山田',
+      furigana_first: 'ジロウ',
+      furigana_last: 'ヤマダ',
+      birthdate: '2010-06-11',
+      tell: '00000000000',
+      password: "jiro123"
+    },
+    {
+      nickname: 'ichiro',
+      email: 'tes3@ichiro.com',
+      first_name: '一郎',
+      last_name: '山田',
+      furigana_first: 'イチロウ',
+      furigana_last: 'ヤマダ',
+      birthdate: '2010-08-11',
+      tell: '00000000000',
+      password: "ichiro1"
+    },
+    {
+      nickname: 'saburo',
+      email: 'test4@saburo.com',
+      first_name: '三郎',
+      last_name: '山田',
+      furigana_first: 'サブロウ',
+      furigana_last: 'ヤマダ',
+      birthdate: '2010-07-11',
+      tell: '00000000000',
+      password: "saburo1"
+    },
+  ]
+)
