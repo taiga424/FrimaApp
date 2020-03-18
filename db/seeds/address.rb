05764a9adb2de8303d60d0e64c61c58d4b9fa9e5
taiga424 <taiga-hand4@ezweb.ne.jp>
2020-03-18 14:12:59 +0900

    add address.rb

---
+++ db/seeds/address.rb
@@ -0,0 +1,36 @@
+Address.create!(
+  [
+    {
+      prefecture: 1,
+      city: '札幌市',
+      number: '1-1-1',
+      building: '',
+      postal_code: 111111,
+      user_id: 1
+    },
+    {
+      prefecture: 2,
+      city: '青森市',
+      number: '2-2-2',
+      building: '',
+      postal_code: 222222,
+      user_id: 2
+    },
+    {
+      prefecture: 3,
+      city: '秋田市',
+      number: '3-3-3',
+      building: '',
+      postal_code: 333333,
+      user_id: 3
+    },
+    {
+      prefecture: 4,
+      city: '盛岡市',
+      number: '4-4-4',
+      building: '',
+      postal_code: 444444,
+      user_id: 4
+    },
+  ]
+)
