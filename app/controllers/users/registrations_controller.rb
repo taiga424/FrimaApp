579d120fa8a475ce7d43180e41d0e9a42636a020
Tomoya-Sonok <wlcmty08kh@gmail.com>
2020-03-20 12:20:43 +0900

    modified the SNS-api-function

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -20,8 +20,17 @@ class Users::RegistrationsController < Devise::RegistrationsController
       session["devise.regist_data"] = {user: @user.attributes}
       session["devise.regist_data"][:user]["password"] = params[:user][:password]
       @address = @user.build_address
-      registration_path(resource_name)
+      render :new_address and return
     end
+    @user = User.new(sign_up_params)
+      unless @user.valid?
+        flash.now[:alert] = @user.errors.full_messages
+        render :new and return
+      end
+    session["devise.regist_data"] = {user: @user.attributes}
+    session["devise.regist_data"][:user]["password"] = params[:user][:password]
+    @address = @user.build_address
+    render :new_address and return
     super
   end
 
@@ -31,13 +40,12 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   def create_address
     @user = User.new(session["devise.regist_data"]["user"])
-    @user.build_address(@address.attributes)
     @address = Address.new(address_params)
     unless @address.valid?
       flash.now[:alert] = @address.errors.full_messages
       render :new_address and return
     end
-    
+    @user.build_address(@address.attributes)
     @user.save
     sign_in(:user, @user)
     redirect_to root_path
